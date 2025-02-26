f8412bdf03998327e2de1d26717e344ce66f744d
Cipher0023 <dani.esparza@hotmail.com>
2025-02-26 15:50:52 -0300

    crated a login system with encripted passwords and started private routes

--- backendfitfat/routes/public.js
+++ backendfitfat/routes/public.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import bcrypt from 'bcrypt'
 import {PrismaClient} from '@prisma/client'
+import jwt from 'jsonwebtoken'
 
 const prisma = new PrismaClient()
-
 const router = express.Router()
+
+const JWT_SECRET = process.env.JWT_SECRET
 
 //Cadastro
 router.post('/cadastro', async(req, res) => {
@@ -27,6 +29,41 @@ router.post('/cadastro', async(req, res) => {
     catch (err) {
         res.status(500).json({message: 'Erro no servidor, tente novamente mais tarde'})
     }
+})
+
+router.post('/login', async(req, res) => {
+
+    try{
+        const userInfo = req.body
+
+
+        //search for user in the database
+        const user = await prisma.user.findUnique({
+            where:{email:userInfo.email},
+        })
+
+        //verify if user exists
+        if(!user){
+            return res.status(404).json({message: 'Usuário não encontrado'})
+        }
+
+        const isMatch = await bcrypt.compare(userInfo.password, user.password)
+
+        //compare the password
+        if(!isMatch){
+            return res.status(400).json({message: 'senha incorreta'})
+        }
+
+        //generate token
+        const token = jwt.sign({id:user.id}, JWT_SECRET, {expiresIn: '7d'})
+
+        res.status(200).json(token)
+    }
+    catch(err){
+        res.status(500).json({message: 'Erro no servidor, tente novamente mais tarde'})
+    }
+
+
 })
 
 export default router
--- backendfitfat/server.js
+++ backendfitfat/server.js
@@ -1,20 +1,16 @@
 import express from 'express'
 import publicRoutes from './routes/public.js'
+import privateRoutes from './routes/private.js'
+import auth from './middlewares/auth.js'
 
 const app = express()
 app.use(express.json())
 
+
+//rotas publicas
 app.use('/',publicRoutes)
 
-/*
-3 rotas
-
-Publicas
-  Cadastro e login
-
-Privadas
-  listar users
-
-*/
+//rotas privadas
+app.use('/', auth ,privateRoutes)
 
 app.listen(3001, () => console.log('Server running on port 3001'))
